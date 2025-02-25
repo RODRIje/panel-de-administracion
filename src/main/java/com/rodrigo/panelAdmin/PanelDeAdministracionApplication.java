e296bb8435ece5327b49273489545b317db3e83f
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2025-02-25 11:45:12 -0300

    commit inicial

---
+++ src/main/java/com/rodrigo/panelAdmin/PanelDeAdministracionApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.panelAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelDeAdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelDeAdministracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/panelAdmin/PanelDeAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.panelAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelDeAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
