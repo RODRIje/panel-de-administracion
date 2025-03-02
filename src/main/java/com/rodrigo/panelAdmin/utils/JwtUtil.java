c1d1313f272650925d75765131f65bcb2c1b31aa
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2025-03-02 20:13:06 -0300

    Se agrego el metodo getUserById

--- src/main/java/com/rodrigo/panelAdmin/utils/JwtUtil.java
+++ src/main/java/com/rodrigo/panelAdmin/utils/JwtUtil.java
@@ -1,7 +1,9 @@
 package com.rodrigo.panelAdmin.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.rodrigo.panelAdmin.entities.User;
 
 import java.util.Date;
@@ -9,9 +11,9 @@ import java.util.Date;
 public class JwtUtil {
 
     private static final String SECRET_KEY = "gj43jng9";
+    private static final Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 
     public static String generateToken(User user){
-        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
         String token = JWT.create()
                 .withIssuer("Rodrigo")
                 .withClaim("userId", user.getId())
@@ -25,5 +27,15 @@ public class JwtUtil {
     private static Date getExpiresDate() {
         return new Date(System.currentTimeMillis()
                 + (1000l * 60 * 60 * 24 * 14));
+    }
+
+    public static String getUserById(String token){
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("Rodrigo")
+                .build();
+
+        DecodedJWT decoded = verifier.verify(token);
+        String userId = decoded.getClaim("userId").asString();
+        return  userId;
     }
 }
