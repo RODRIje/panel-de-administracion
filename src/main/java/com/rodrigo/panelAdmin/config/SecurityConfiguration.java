595dfea2a128b5bc80fa2286530ab4c3ae78a6e7
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2025-03-02 22:30:23 -0300

    Se agrego la clase SecurityConfiguration

--- src/main/java/com/rodrigo/panelAdmin/config/SecurityConfiguration.java
+++ src/main/java/com/rodrigo/panelAdmin/config/SecurityConfiguration.java
@@ -1,2 +1,25 @@
-package com.rodrigo.panelAdmin.config;public class SecurityConfiguration {
+package com.rodrigo.panelAdmin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(request ->{
+            request.requestMatchers("/api/register", "/api/login").permitAll();
+            request.requestMatchers("/api/**").authenticated();
+        });
+
+        return http.build();
+    }
 }
